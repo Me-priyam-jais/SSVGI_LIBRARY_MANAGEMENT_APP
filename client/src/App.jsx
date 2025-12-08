9450286d35e3b9dd79ab0c08d12749a899ded8b3
Priyam Jaiswal <priyamj374@gmail.com>
2025-12-08 14:20:21 +0530

    First commit form this branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import ForgotPassword from "./pages/ForgotPassword";
-import Register from "./pages/Register";
-import OTP from "./pages/OTP";
-import ResetPassword from "./pages/ResetPassword";
+import Home from "./pages/Home.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import Login from "./pages/Login.jsx";
+import OTP from "./pages/OTP.jsx";
+import Register from "./pages/Register.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx";
 import { ToastContainer } from "react-toastify";
-import { useDispatch, useSelector } from "react-redux";
-import { getUser } from "./store/slices/authSlice";
-import { fetchAllUsers } from "./store/slices/userSlice";
-
 const App = () => {
-  const { user, isAuthenticated } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getUser());
-    if (isAuthenticated && user?.role === "Admin") {
-      dispatch(fetchAllUsers());
-    }
-  }, [isAuthenticated]);
   return (
     <Router>
       <Routes>
@@ -27,10 +15,10 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/password/forgot" element={<ForgotPassword />} />
-        <Route path="/otp-verification/:email" element={<OTP />} />
+        <Route path="/otp-verification" element={<OTP />} />
         <Route path="/password/reset/:token" element={<ResetPassword />} />
       </Routes>
-      <ToastContainer theme="dark" />
+      <ToastContainer />
     </Router>
   );
 };
--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,53 +1,7 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import Header from "../layout/Header";
 
 const Users = () => {
-  const { user } = useSelector((state) => state.user);
-
-  const formatDate = (timeStamp) => {
-    const date = new Date(timeStamp);
-    const formattedDate = `${String(date.getDate()).padStart(2, "0")}-${String(
-      date.getMonth() + 1
-    ).padStart(2, "0")}-${String(date.getFullYear())}`;
-    const formattedTime = `${String(date.getHours()).padStart(2, "0")}:${String(
-      date.getMinutes()
-    ).padStart(2, "0")}:${String(date.getSeconds()).padStart(2, "0")}`;
-    const result = "${formattedDate} ${formattedTime}";
-    return result;
-  };
-
-  return (
-    <>
-      <main className="relative flex-1 p-6 pt-28">
-        <Header />
-        {/* Sub Header */}
-        <header className="flex flex-col gap-3 md:flex-row md:justify-between md:items-center">
-          <h2 className="text-xl font-medium md:text-2xl md:font-semibold">
-            Registered Users
-          </h2>
-        </header>
-
-        {/* Table */}
-        {users && users.filter((u) => u.role === "User").length > 0 ? (
-          <div className="mt-6 overflow-auto bg-white rounded-md shadow-lg">
-            <table className="min-w-full border-collapse">
-              <thead>
-                <tr className="bg-gray-200">
-                  <th className="px-4 py-2 text-left">ID</th>
-                  <th className="px-4 py-2 text-left">Name</th>
-                  <th className="px-4 py-2 text-left">Email</th>
-                  <th className="px-4 py-2 text-left">Role</th>
-                </tr>
-              </thead>
-            </table>
-          </div>
-        ) : (
-          <></>
-        )}
-      </main>
-    </>
-  );
+  return <></>;
 };
 
 export default Users;

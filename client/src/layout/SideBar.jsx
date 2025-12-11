9d6cb0a4988eb20ef1906ebcfd1d0d553a38659d
thepiyush005 <piyushbajpai006@gmail.com>
2025-12-11 22:53:48 +0530

    full commit

--- client/src/layout/SideBar.jsx
+++ client/src/layout/SideBar.jsx
@@ -103,7 +103,7 @@ const SideBar = ({ isSideBarOpen, setIsSideBarOpen, setSelectedComponent }) => {
             </>
           )}
           <button
-            className="w-full py-2 font-medium bg-transparent rounded-md hover:cursor-pointer flex items-center space-x-2"
+            className="md:hidden w-full py-2 font-medium bg-transparent rounded-md hover:cursor-pointer flex items-center space-x-2"
             onClick={() => dispatch(toggleSettingPopup())}
           >
             <img src={settingIcon} alt="icon" />
